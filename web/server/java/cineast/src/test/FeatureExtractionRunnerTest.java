d979ab95f97c6f9d3899e9555c026a24d292828b
Bacluc <lucius.bachmann@gmx.ch>
2017-03-28 17:02:24 +0200

    accept command line arguments

--- web/server/java/cineast/src/test/FeatureExtractionRunnerTest.java
+++ web/server/java/cineast/src/test/FeatureExtractionRunnerTest.java
@@ -8,8 +8,24 @@ import cineast.core.run.FeatureExtractionRunner;
 public class FeatureExtractionRunnerTest {
 
 	public static void main(String[] args) {
+            
+                String targetpath = "/var/www/html/filigrana/web/client/img/photos/28_03_2017";
+                String sourcepath = "/var/www/html/filigrana/web/data/iphclasses";
+                boolean watermark = false;
+                if(args.length >0){
+                    targetpath = args[0];
+                }
+                if(args.length > 1){
+                    sourcepath = args[1];
+                }
+                String s = new String(new char[]{'1'});
+                if(args.length > 2){
+                    if(args[2].equals(s)){
+                        watermark = true;
+                    }
+                }
 		
-		FeatureExtractionRunner runner = new FeatureExtractionRunner("/var/www/html/filigrana/web/client/img/photos/11_11_2014", "/var/www/html/filigrana/web/client/img/test/testimagetmp.png", true,false, 129);
+		FeatureExtractionRunner runner = new FeatureExtractionRunner(targetpath, sourcepath, watermark,false);
 		
 		long globalStart = System.currentTimeMillis();
 		System.err.println("beginning extraction");
@@ -25,3 +41,4 @@ public class FeatureExtractionRunnerTest {
 	}
 
 }
+
